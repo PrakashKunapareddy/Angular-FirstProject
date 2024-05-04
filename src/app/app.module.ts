df42e353e7402a4c42299c1e2c7d6324d42accbe
PrakashKunapareddy <154238778+PrakashKunapareddy@users.noreply.github.com>
2024-05-04 13:46:18 +0530

    angular?FirstApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { ErrorMessageComponent } from './error-message/error-message.component';
+import { SuccessMessageComponent } from './success-message/success-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    ErrorMessageComponent,
+    SuccessMessageComponent
+   
   ],
   imports: [
     BrowserModule
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server', // --using as HTMl tag
+    // selector: '[app-server]', --using as attribute 
+    // selector: '.app-server', --using as class
+
+    templateUrl:'./server.component.html'
+})
+export class ServerComponent{
+
+}
